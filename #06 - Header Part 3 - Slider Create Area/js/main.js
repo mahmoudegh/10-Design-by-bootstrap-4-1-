2ce47e5f88736bbb06980cd7ed314316a2e7f0b9
mahmoudegh <48510315+mahmoudegh@users.noreply.github.com>
2021-03-24 16:52:32 +0200

    Design by bootstrap 4 (1)

---
+++ #06 - Header Part 3 - Slider Create Area/js/main.js
@@ -0,0 +1,9 @@
+// Not Working
+// $(function () {
+//   'use strict';
+//   // Adjust Slider Height
+//   var winH   = $(window).height(),
+//       upperH = $(' .upper-bar').innerHeight(),
+//       navH   = $(' .navbar').innerHeight();
+//   $(' .carousel').height(winH - (upperH + navH));
+// });
---
+++ #07 - Header Part 4 - Add Slider Images/js/main.js
@@ -0,0 +1,9 @@
+// // Not Working
+// $(function () {
+//    'use strict';
+//    // Adjust Slider Height
+//    var winH   = $(window).height(),
+//        upperH = $('.upper-bar').innerHeight(),
+//        navH   = $('.navbar').innerHeight();
+//    $('.slider, .carousel-item').height(winH - ( upperH + navH ));
+//  });
